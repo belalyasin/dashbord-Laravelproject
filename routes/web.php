9c659b0138ea0d18bf03fdf0fedd664a6f209069
belalyasin <76963041+belalyasin@users.noreply.github.com>
2022-03-03 12:23:29 +0200

    Rout-view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('cms/admin')->group(function () {
+    Route::view('/', 'cms.dashboard');
 });
